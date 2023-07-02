df89b3166f441e57552136ed60a25990415df640
Pasan Bopegamage <pasanb2001@gmail.com>
2023-07-02 14:33:54 +0530

    Add files via upload

---
+++ NewStack/NewStack/NewStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewStackApp.swift
+//  NewStack
+//
+//  Created by Pasan Bopagamage on 2023-06-22.
+//
+
+import SwiftUI
+
+@main
+struct NewStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
